6b841067ba71ca16d10cec9855d177d0b65f64e8
Agustin Galante <agustin.galante.19@outlook.es>
2025-04-01 08:27:01 -0300

    basic api

---
+++ src/api/index.ts
@@ -0,0 +1,36 @@
+import express, { type Application } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const PORT = process.env.PORT ?? 5000;
+
+class NotifyMeApi {
+	private app: Application;
+
+	constructor() {
+		this.app = express();
+		this.routes();
+		this.config();
+	}
+
+	private config() {
+		this.app.use(cors());
+		this.app.use(morgan("dev"));
+		this.app.use(express.urlencoded({ extended: true }));
+		this.app.use(express.json());
+	}
+
+	private routes() {
+		this.app.get("/", (_, res) => {
+			res.send("hi");
+		});
+	}
+
+	start() {
+		this.app.listen(PORT, () => {
+			console.log(`Server running: http://localhost:${PORT}`);
+		});
+	}
+}
+
+export const notifyMeApiInstance = new NotifyMeApi();
