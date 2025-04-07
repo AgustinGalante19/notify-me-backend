5c9512ac66094bf705f2edfa7c738851a73b747d
Agustin Galante <agustin.galante.19@outlook.es>
2025-04-07 08:38:09 -0300

    fix removing padstart from get day tomorrow

--- src/utils/getTomorrowDay.ts
+++ src/utils/getTomorrowDay.ts
@@ -1,5 +1,5 @@
 export default function getTomorrowDay(): string {
 	const tomorrow = new Date();
 	tomorrow.setDate(tomorrow.getDate() + 1);
-	return String(tomorrow.getDate()).padStart(2, "0");
+	return String(tomorrow.getDate());
 }
